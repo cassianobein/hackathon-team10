9040d4b83acc39789636ff977807955530b3a2df
charlielittle <charlie.little@mongodb.com>
2022-03-15 10:34:37 -0400

    added env vars for Atlas

--- fiftyone-load-dataset-by-name.py
+++ fiftyone-load-dataset-by-name.py
@@ -1,21 +1,32 @@
+import os
+#config defaults (or place these in ~/.fiftyone/config.json)
+os.environ['FIFTYONE_DATABASE_URI'] = 'mongodb+srv://main_user:musermuser@team10.3b42v.mongodb.net/fiftyone?retryWrites=true&w=majority'
+os.environ['FIFTYONE_DATABASE_VALIDATION'] = "false"
+
 import fiftyone as fo
 import fiftyone.zoo as foz
 
-#config defaults (or place these in ~/.fiftyone/config.json)
-fo.config.database_uri =        "mongodb+srv://main_user:musermuser@team10.3b42v.mongodb.net/fiftyone?retryWrites=true&w=majority"
-fo.config.database_validation = False
 fo.config.dataset_zoo_dir =     "./datasets"
 fo.config.default_dataset_dir = "./datasets"
 
 dataset_name = "coco-2017"
 dataset_dir = fo.get_default_dataset_dir( dataset_name )
+
 print( dataset_dir )
+print( "=========> BEFORE <===========" )
+print( fo.config )
 dataset = foz.load_zoo_dataset(
     dataset_name,
     split="validation",
     max_samples=5000,
-    cleanup=False
+    drop_existing_dataset=True,
+    overwrite=True
 )
+
+dataset.persistent = True
+dataset.save()
+print( "=========> AFTER <===========" )
+print( fo.config )
 
 # dataset = foz.load_zoo_dataset( dataset_name, cleanup=False )
 session = fo.launch_app(dataset)
